2730edf76e0009fa253e9a406c775fd72bae7d27
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-09-04 13:09:51 +0000

    kill  me

--- main/utils.py
+++ main/utils.py
@@ -5,7 +5,7 @@ import aiofiles
 import json
 from spin import Spinner
 
-async def log(message: str, level, log_file: str = "/workspaces/JARVIS-Test/main/logs/log.txt", append = True):
+async def log(message: str, level, log_file: str = "/workspaces/JARVIS-Test/main/logs/log.log", append = True):
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     emoji = {
         "info": "ℹ️",
@@ -17,21 +17,3 @@ async def log(message: str, level, log_file: str = "/workspaces/JARVIS-Test/main
     async with aiofiles.open(log_file, "a" if append else "w") as f:
         print(text)
         await f.write(text)
-
-
-async def main():
-    s = Spinner("⚙️  Spinning up Pulse AI...")
-    spinner_task = asyncio.create_task(s.spinny_thingy())
-
-    from models import main as pulse_main
-
-    await pulse_main()
-    s.change_text("NEW")
-    await pulse_main()
-
-    s.set_done()
-    await spinner_task
-    print("✅ Spinny done!\n")
-
-if __name__ == "__main__":
-    asyncio.run(main())
