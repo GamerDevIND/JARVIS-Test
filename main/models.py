bcc9200f040868e6b508deb0292d016fa0ea7c89
GamerDevIND <141837560+GamerDevIND@users.noreply.github.com>
2025-09-25 12:25:05 +0000

    ahw man

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ import subprocess
 from utils import log
 
 class Model:
-    def __init__(self, role: str, name: str, ollama_name: str, has_tools: bool, has_CoT: bool, has_vision:bool, port: int, system_prompt: str) -> None:
+    def __init__(self, role: str, name: str, ollama_name: str, has_tools: bool, has_CoT: bool, has_vision:bool, port: int, system_prompt: str):
         self.role = role
         self.name = name
         self.ollama_name = ollama_name
@@ -102,6 +102,7 @@ class Model:
                                 json_line = json.loads(line.strip())
                                 # Assumes a streaming response contains a "message" object
                                 if 'message' in json_line:
+                                    print(json_line)
                                     break
                             except json.JSONDecodeError:
                                 continue
